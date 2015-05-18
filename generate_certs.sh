95cd877c368173e3e2bea80b0a70c2304cfcbe88
jmreicha <josh@josh-macbook-air.local>
2015-05-17 22:57:59 -0500

    Add option to specify domain for cert

--- generate_certs.sh
+++ generate_certs.sh
@@ -9,17 +9,22 @@ logstash_crt=./logstash/logstash-forwarder.crt
 function make_certs {
     openssl req -x509 -days $cert_days -batch -nodes -newkey rsa:2048 \
     -keyout $logstash_key \
-    -out $logstash_crt
+    -out $logstash_crt \
+    -subj /CN=$domain_name
 }
 
 # Check if both certs are present
 if [ ! -f $logstash_key ] && [ ! -f $logstash_crt ] ; then
     echo "Generating self signed certs for logstash"
+    echo "Enter fqdn for cert"
+    read domain_name
     make_certs
     echo "Done"
 elif [ ! -f $logstash_key ] || [ ! -f $logstash_crt ] ; then
     echo "Cert missing. Removing old certs and regenerating."
     rm -f ./logstash/logstash-forwarder*
+    echo "Enter fqdn for cert"
+    read domain_name
     make_certs
 else
     echo "Certificates already exist!"
