80f3015f46af65b388faa646f75b8c354cbe2976
jmreicha <josh@josh-macbook-air.local>
2015-05-17 22:46:16 -0500

    Clean up generate_certs script

--- generate_certs.sh
+++ generate_certs.sh
@@ -2,18 +2,22 @@
 
 set -e 
 
+cert_days=3650
+logstash_key=./logstash/logstash-forwarder.key
+logstash_crt=./logstash/logstash-forwarder.crt
+
 function make_certs {
-    openssl req -x509 -days 3650 -batch -nodes -newkey rsa:2048 \
-    -keyout ./logstash/logstash-forwarder.key \
-    -out ./logstash/logstash-forwarder.crt
+    openssl req -x509 -days $cert_days -batch -nodes -newkey rsa:2048 \
+    -keyout $logstash_key \
+    -out $logstash_crt
 }
 
 # Check if both certs are present
-if [ ! -f ./logstash/logstash-forwarder.key ] && [ ! -f ./logstash/logstash-forwarder.crt ] ; then
+if [ ! -f $logstash_key ] && [ ! -f $logstash_crt ] ; then
     echo "Generating self signed certs for logstash"
     make_certs
     echo "Done"
-elif [ ! -f ./logstash/logstash-forwarder.key ] || [ ! -f ./logstash/logstash-forwarder.crt ] ; then
+elif [ ! -f $logstash_key ] || [ ! -f $logstash_crt ] ; then
     echo "Cert missing. Removing old certs and regenerating."
     rm -f ./logstash/logstash-forwarder*
     make_certs
